f3e9501bcfe8e40a352debb461739612691fe1ac
Sébastien Labbé <slabqc@gmail.com>
2017-01-17 16:07:03 +0100

    fixing install file

--- install.sh
+++ install.sh
@@ -3,18 +3,24 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# vim
+ln -s ${BASEDIR}/vimrc ~/.vimrc
+
+# bug next line is not idempotent: directory vim contains directory vim ...
+ln -s ${BASEDIR}/vim/ ~/.vim
+
+# vim spell files (big files not under version control)
+wget http://ftp.vim.org/vim/runtime/spell/fr.utf-8.spl
+wget http://ftp.vim.org/vim/runtime/spell/fr.utf-8.sug
+mv fr.utf-8.spl vim/spell/
+mv fr.utf-8.sug vim/spell/
+
 # bash
 ln -s ${BASEDIR}/bash_aliases ~/.bash_aliases
 ln -s ${BASEDIR}/bash_profile ~/.bash_profile
 ln -s ${BASEDIR}/bashrc ~/.bashrc
 ln -s ${BASEDIR}/dir_colors ~/.dir_colors
 ln -s ${BASEDIR}/inputrc ~/.inputrc
-
-# vim
-ln -s ${BASEDIR}/vimrc ~/.vimrc
-
-# bug next line is not idempotent: directory vim contains directory vim ...
-ln -s ${BASEDIR}/vim/ ~/.vim
 
 # tmux
 ln -s ${BASEDIR}/tmux.conf ~/.tmux.conf
@@ -23,16 +29,11 @@ ln -s ${BASEDIR}/tmux.conf ~/.tmux.conf
 ln -s ${BASEDIR}/gitconfig ~/.gitconfig
 
 # hg
-hgrc
+ln -s ${BASEDIR}/hgrc ~/.hgrc
 
 # tikz2pdf
-tikz2pdf.tex
-
-# vim spell files (big files not under version control)
-wget http://ftp.vim.org/vim/runtime/spell/fr.utf-8.spl
-wget http://ftp.vim.org/vim/runtime/spell/fr.utf-8.sug
-mv fr.utf-8.spl vim/spell/
-mv fr.utf-8.sug vim/spell/
+ln -s ${BASEDIR}/tikz2pdf.tex ~/.tikz2pdf.tex
 
 # Done
-echo "Done installing dotfiles"
+echo "Done installing dotfiles (unless they some were already there!)"
+echo "... you should now check whether some config files are missing."
