696a0c32ea7842695f0e76598bd0bbefcbd4eb76
Sébastien Labbé <slabqc@gmail.com>
2017-01-17 15:14:10 +0100

    first working version of install.sh

--- install.sh
+++ install.sh
@@ -3,18 +3,34 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# bash
+ln -s ${BASEDIR}/bash_aliases ~/.bash_aliases
+ln -s ${BASEDIR}/bash_profile ~/.bash_profile
+ln -s ${BASEDIR}/bashrc ~/.bashrc
+ln -s ${BASEDIR}/dir_colors ~/.dir_colors
+ln -s ${BASEDIR}/inputrc ~/.inputrc
+
 # vim
 ln -s ${BASEDIR}/vimrc ~/.vimrc
 ln -s ${BASEDIR}/vim/ ~/.vim
 
-# zsh
-ln -s ${BASEDIR}/zshrc ~/.zshrc
+# tmux
+ln -s ${BASEDIR}/tmux.conf ~/.tmux.conf
 
 # git
 ln -s ${BASEDIR}/gitconfig ~/.gitconfig
 
-# spell (big files not under version control)
+# hg
+hgrc
+
+# tikz2pdf
+tikz2pdf.tex
+
+# vim spell files (big files not under version control)
 wget http://ftp.vim.org/vim/runtime/spell/fr.utf-8.spl
 wget http://ftp.vim.org/vim/runtime/spell/fr.utf-8.sug
 mv fr.utf-8.spl vim/spell/
 mv fr.utf-8.sug vim/spell/
+
+# Done
+echo "Done installing dotfiles"
